f26fb2abca05d3d34c3b38318cca53cdb311404c
Axel Jacobsen <axelnj44@gmail.com>
2022-05-08 13:57:38 -0700

    most of the way there, but bugs exist

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,13 @@ impl<'a> IntoIterator for &'a ImageData {
                         b: chunk[2],
                         a: 255,
                     },
-                    _ => PixelValue {
+                    4 => PixelValue {
                         r: chunk[0],
                         g: chunk[1],
                         b: chunk[2],
                         a: chunk[3],
                     },
+                    _ => panic!("we can't handle grayscale!"),
                 }),
         )
     }
@@ -59,29 +60,6 @@ fn get_pixel_size(color_type: png::ColorType) -> usize {
     }
 }
 
-fn get_qoif_header(id: &ImageData) -> [u8; 14] {
-    let channels = get_pixel_size(id.png_type);
-    let colorspace = id.is_srgb;
-    let width = id.width.to_ne_bytes();
-    let height = id.height.to_ne_bytes();
-    [
-        0x71,
-        0x6F,
-        0x69,
-        0x66,
-        width[0],
-        width[1],
-        width[2],
-        width[3],
-        height[0],
-        height[1],
-        height[2],
-        height[3],
-        channels as u8,
-        if id.is_srgb { 0 } else { 1 },
-    ]
-}
-
 #[derive(Clone, PartialEq, Eq)]
 struct PixelValue {
     r: u8,
@@ -91,7 +69,12 @@ struct PixelValue {
 }
 
 fn hash_rgba(px: &PixelValue) -> usize {
-    ((3 * px.r + 5 * px.g + 7 * px.b + 11 * px.a) % 64) as usize
+    // use Wrapping? probably faster?
+    ((3_u64 * (px.r as u64)
+        + 5_u64 * (px.g as u64)
+        + 7_u64 * (px.b as u64)
+        + 11_u64 * (px.a as u64))
+        % 64) as usize
 }
 
 // i know that this is also garb but please be kind (for now)
@@ -120,11 +103,106 @@ fn get_file_data() -> Result<ImageData, png::DecodingError> {
     })
 }
 
+fn pixel_previously_seen(p: &PixelValue, prev_seen: &Vec<PixelValue>) -> Option<usize> {
+    let pixel_hash = hash_rgba(p);
+    if prev_seen[pixel_hash] == *p {
+        Some(pixel_hash)
+    } else {
+        None
+    }
+}
+
+fn pixel_qoi_diff(prev_pixel: &PixelValue, cur_pixel: &PixelValue) -> Option<[u8; 3]> {
+    if prev_pixel.a != cur_pixel.a {
+        return None;
+    }
+    let dr = (cur_pixel.r as i16) - (prev_pixel.r as i16);
+    let dg = (cur_pixel.g as i16) - (prev_pixel.g as i16);
+    let db = (cur_pixel.b as i16) - (prev_pixel.b as i16);
+    let dx_range: core::ops::Range<i16> = -2..2;
+    if dx_range.contains(&dr) && dx_range.contains(&dg) && dx_range.contains(&db) {
+        Some([(dr + 2) as u8, (dg + 2) as u8, (db + 2) as u8])
+    } else {
+        None
+    }
+}
+
+fn pixel_luma_diff(prev_pixel: &PixelValue, cur_pixel: &PixelValue) -> Option<[u8; 3]> {
+    if prev_pixel.a != cur_pixel.a {
+        return None;
+    }
+    let dr = (cur_pixel.r as i16) - (prev_pixel.r as i16);
+    let dg = (cur_pixel.g as i16) - (prev_pixel.g as i16);
+    let db = (cur_pixel.b as i16) - (prev_pixel.b as i16);
+    let dr_dg = dr - dg;
+    let db_dg = db - dg;
+    let dg_range = -32..32;
+    let drdb_range = -8..8;
+    if dg_range.contains(&dg) && drdb_range.contains(&dr_dg) && drdb_range.contains(&db_dg) {
+        Some([(dr + 8) as u8, (dg + 32) as u8, (db + 8) as u8])
+    } else {
+        None
+    }
+}
+
+fn get_qoif_header(id: &ImageData) -> [u8; 14] {
+    let channels = get_pixel_size(id.png_type);
+    let colorspace = id.is_srgb;
+    let width = id.width.to_ne_bytes();
+    let height = id.height.to_ne_bytes();
+    [
+        0x71,
+        0x6F,
+        0x69,
+        0x66,
+        width[0],
+        width[1],
+        width[2],
+        width[3],
+        height[0],
+        height[1],
+        height[2],
+        height[3],
+        channels as u8,
+        if id.is_srgb { 0 } else { 1 },
+    ]
+}
+
+fn get_qoif_footer() -> [u8; 8] {
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]
+}
+
+fn encode_qoip_rgb(p: &PixelValue) -> [u8; 4] {
+    [0xFE, p.r, p.g, p.b]
+}
+
+fn encode_qoip_rgba(p: &PixelValue) -> [u8; 5] {
+    [0xFF, p.r, p.g, p.b, p.a]
+}
+
+fn encode_run(run_length: u8) -> [u8; 1] {
+    assert!(run_length < 63, "run_length greater than 62!");
+    [0xC0 | run_length - 1]
+}
+
+fn encode_idx(idx: usize) -> [u8; 1] {
+    assert!(idx < 64, "index must be in 0..63");
+    [0x00 | idx as u8]
+}
+
+fn encode_diff(dr: u8, dg: u8, db: u8) -> [u8; 1] {
+    [0x40 | dr << 4 | dg << 2 | db << 0]
+}
+
+fn encode_luma(dg: u8, drdg: u8, dbdg: u8) -> [u8; 2] {
+    [0x80 | dg, drdg << 4 | dbdg << 0]
+}
+
 fn encode_qoif(png_data: ImageData) -> std::io::Result<()> {
-    let out_fname = "out.qf";
+    let out_fname = "out.qoi";
     let mut out_file = File::create(out_fname)?;
 
-    out_file.write(&get_qoif_header(&png_data));
+    out_file.write_all(&get_qoif_header(&png_data));
 
     let pixel_size = get_pixel_size(png_data.png_type);
 
@@ -140,34 +218,44 @@ fn encode_qoif(png_data: ImageData) -> std::io::Result<()> {
             r: 0,
             g: 0,
             b: 0,
-            a: 255
+            a: 0
         };
         64
     ];
 
     let pixel_size = get_pixel_size(png_data.png_type);
 
-    let mut in_a_run: bool = false;
     let mut prev_run: u8 = 0;
 
     for pixel in png_data.into_iter() {
-        // check for a run
-        if pixel == prev_pixel {
-            in_a_run = true;
+        if pixel == prev_pixel && prev_run < 62 {
+            // check for a run
             prev_run += 1;
-        } else if in_a_run {
-            in_a_run = false;
-            prev_run; // write!
-            out_file.write_all([]);
-        } else {
+        } else if prev_run > 0 {
+            // handle leaving a run
+            prev_run = 0;
+            out_file.write_all(&encode_run(prev_run));
+        } else if let Some(idx) = pixel_previously_seen(&pixel, &prev_pixel_arr) {
+            // check for idx
+            out_file.write_all(&encode_idx(idx));
+        } else if let Some([dr, dg, db]) = pixel_qoi_diff(&prev_pixel, &pixel) {
             // check for diff
+            out_file.write_all(&encode_diff(dr, dg, db));
+        } else if let Some([dg, drdg, dbdg]) = pixel_luma_diff(&prev_pixel, &pixel) {
             // check for luma
-            // default to RGB or RGBA
-        };
-
-
+            out_file.write_all(&encode_luma(dg, drdg, dbdg));
+        } else {
+            if pixel_size == 3 {
+                out_file.write_all(&encode_qoip_rgb(&pixel));
+            } else if pixel_size == 4 {
+                out_file.write_all(&encode_qoip_rgba(&pixel));
+            } else {
+                panic!("How did you get here?!");
+            }
+        }
         prev_pixel_arr[hash_rgba(&pixel)] = pixel.clone();
     }
+    out_file.write_all(&get_qoif_footer());
     Ok(())
 }
 
