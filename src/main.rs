213fd7e65d5ff935fed70bcac8a7b2044aa5fc72
Axel Jacobsen <axelnj44@gmail.com>
2022-05-16 11:16:44 -0700

    attemping testing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::fmt;
 use std::fs::File;
+use std::io::BufWriter;
 use std::io::Read;
 use std::io::Write;
-use std::io::BufWriter;
 use std::path::Path;
+
+/* Slow and non-idomatic */
 
 // i know that it is garb to do this but oh well
 struct ImageData {
@@ -163,15 +165,19 @@ fn get_pixel_luma_diff(prev_pixel: &PixelValue, cur_pixel: &PixelValue) -> Optio
     if prev_pixel.a != cur_pixel.a {
         return None;
     }
-    let dr = (cur_pixel.r as i16) - (prev_pixel.r as i16);
-    let dg = (cur_pixel.g as i16) - (prev_pixel.g as i16);
-    let db = (cur_pixel.b as i16) - (prev_pixel.b as i16);
-    let dr_dg = dr - dg;
-    let db_dg = db - dg;
+    let dr = (cur_pixel.r as i8).wrapping_sub(prev_pixel.r as i8);
+    let dg = (cur_pixel.g as i8).wrapping_sub(prev_pixel.g as i8);
+    let db = (cur_pixel.b as i8).wrapping_sub(prev_pixel.b as i8);
+    let dr_dg = dr.wrapping_sub(dg);
+    let db_dg = db.wrapping_sub(dg);
     let dg_range = -32..32;
     let drdb_range = -8..8;
     if dg_range.contains(&dg) && drdb_range.contains(&dr_dg) && drdb_range.contains(&db_dg) {
-        Some([(dr + 8) as u8, (dg + 32) as u8, (db + 8) as u8])
+        Some([
+            (dr as u8).wrapping_add(8),
+            (dg as u8).wrapping_add(32),
+            (db as u8).wrapping_add(8),
+        ])
     } else {
         None
     }
@@ -212,7 +218,7 @@ fn encode_qoip_rgba(p: &PixelValue) -> [u8; 5] {
 }
 
 fn encode_run(run_length: u8) -> [u8; 1] {
-    assert!(run_length < 63, "run_length greater than 62!");
+    assert!(0 < run_length && run_length < 63, "run_length invalid");
     [0xC0 | run_length - 1]
 }
 
@@ -229,13 +235,12 @@ fn encode_luma(dg: u8, drdg: u8, dbdg: u8) -> [u8; 2] {
     [0x80 | dg, drdg << 4 | dbdg << 0]
 }
 
-fn encode_qoif(png_data: ImageData) -> std::io::Result<()> {
-    let out_fname = "out.qoi";
-    let mut out_file = File::create(out_fname)?;
+fn calc_qoif(png_data: ImageData) -> std::io::Result<Vec<u8>> {
+    let mut out_file: Vec<u8> = vec![];
 
-    out_file.write_all(&create_qoif_header(&png_data))?;
+    out_file.extend(&create_qoif_header(&png_data));
 
-    let prev_pixel = PixelValue {
+    let mut prev_pixel = PixelValue {
         r: 0,
         g: 0,
         b: 0,
@@ -259,35 +264,46 @@ fn encode_qoif(png_data: ImageData) -> std::io::Result<()> {
         if pixel == prev_pixel && prev_run < 62 {
             // check for a run
             prev_run += 1;
+            continue;
         } else if prev_run > 0 {
             // handle leaving a run
+            out_file.extend(&encode_run(prev_run));
             prev_run = 0;
-            out_file.write_all(&encode_run(prev_run))?;
-        } else if let Some(idx) = get_pixel_index(&pixel, &prev_pixel_arr) {
+        }
+        if let Some(idx) = get_pixel_index(&pixel, &prev_pixel_arr) {
             // check for idx
-            out_file.write_all(&encode_idx(idx))?;
+            out_file.extend(&encode_idx(idx));
         } else if let Some([dr, dg, db]) = get_pixel_qoi_diff(&prev_pixel, &pixel) {
             // check for diff
-            out_file.write_all(&encode_diff(dr, dg, db))?;
-        } else if let Some([dg, drdg, dbdg]) = get_pixel_luma_diff(&prev_pixel, &pixel) {
+            out_file.extend(&encode_diff(dr, dg, db));
+        } else if let Some([drdg, dg, dbdg]) = get_pixel_luma_diff(&prev_pixel, &pixel) {
             // check for luma
-            out_file.write_all(&encode_luma(dg, drdg, dbdg))?;
+            out_file.extend(&encode_luma(dg, drdg, dbdg));
         } else {
             if pixel_size == 3 {
-                out_file.write_all(&encode_qoip_rgb(&pixel))?;
+                out_file.extend(&encode_qoip_rgb(&pixel));
             } else if pixel_size == 4 {
-                out_file.write_all(&encode_qoip_rgba(&pixel))?;
+                out_file.extend(&encode_qoip_rgba(&pixel));
             } else {
-                panic!("How did you get here?!");
+                panic!("How did you get here!");
             }
         }
+
         prev_pixel_arr[hash_rgba(&pixel)] = pixel.clone();
+        prev_pixel = pixel;
     }
     if prev_run > 0 {
-        out_file.write_all(&encode_run(prev_run))?;
+        out_file.extend(&encode_run(prev_run));
     }
-    out_file.write_all(&create_qoif_footer())?;
-    Ok(())
+    out_file.extend(&create_qoif_footer());
+    Ok(out_file)
+}
+
+fn encode_qoif(png_data: ImageData) -> std::io::Result<()> {
+    let out_fname = "out.qoi";
+    let mut out_file = File::create(out_fname)?;
+    let v = calc_qoif(png_data).unwrap();
+    out_file.write_all(&v)
 }
 
 fn grab_n<T: Clone>(itr: &mut std::slice::Iter<T>, n: u8) -> Option<Vec<T>> {
@@ -335,7 +351,7 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
     let is_rgba = pixel_size == 4;
 
     // way to size this?
-    let mut write_array = vec![];
+    let mut write_array: Vec<u8> = vec![];
 
     let end_of_data = qoi_data.bytes.len() - 8;
     let mut qoi_iter = qoi_data.bytes[..end_of_data].iter();
@@ -348,9 +364,9 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
             write_array.push(rgb[2]);
             prev_pixel = PixelValue {
                 r: rgb[0],
-                g:rgb[1],
-                b:rgb[2],
-                a: 255
+                g: rgb[1],
+                b: rgb[2],
+                a: 255,
             };
             prev_pixel_arr[hash_rgba(&prev_pixel)] = prev_pixel.clone();
         } else if next_byte == 0xFF {
@@ -361,13 +377,13 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
             write_array.push(rgba[3]);
             prev_pixel = PixelValue {
                 r: rgba[0],
-                g:rgba[1],
-                b:rgba[2],
-                a: rgba[3]
+                g: rgba[1],
+                b: rgba[2],
+                a: rgba[3],
             };
             prev_pixel_arr[hash_rgba(&prev_pixel)] = prev_pixel.clone();
         } else if next_byte >> 6 == 0b00 {
-            let idx = 0b00111111 | next_byte;
+            let idx = 0b00111111 & next_byte;
             let p = &prev_pixel_arr[idx as usize];
             write_array.push(p.r);
             write_array.push(p.g);
@@ -377,12 +393,12 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
             };
             prev_pixel = prev_pixel_arr[idx as usize].clone();
         } else if next_byte >> 6 == 0b01 {
-            let dr = 0b00110000 | next_byte;
-            let dg = 0b00001100 | next_byte;
-            let db = 0b00000011 | next_byte;
-            let r = prev_pixel.r + dr - 2;
-            let g = prev_pixel.g + dg - 2;
-            let b = prev_pixel.b + db - 2;
+            let dr = 0b00110000 & next_byte;
+            let dg = 0b00001100 & next_byte;
+            let db = 0b00000011 & next_byte;
+            let r = prev_pixel.r.wrapping_add(dr).wrapping_sub(2);
+            let g = prev_pixel.g.wrapping_add(dg).wrapping_sub(2);
+            let b = prev_pixel.b.wrapping_add(db).wrapping_sub(2);
             write_array.push(r);
             write_array.push(g);
             write_array.push(b);
@@ -393,19 +409,19 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
                 r: r,
                 g: g,
                 b: b,
-                a: prev_pixel.a
+                a: prev_pixel.a,
             };
             prev_pixel_arr[hash_rgba(&prev_pixel)] = prev_pixel.clone();
         } else if next_byte >> 6 == 0b10 {
             let drdg_dbdg = qoi_iter.next().expect("guh!");
-            let drdg = 0xF0 | drdg_dbdg;
-            let dbdg = 0x0F | drdg_dbdg;
-            let dg = (0b00111111 | next_byte) - 32;
-            let dr = drdg + dg - 8;
-            let db = dbdg + dg - 8;
-            let r = prev_pixel.r + dr;
-            let g = prev_pixel.g + dg;
-            let b = prev_pixel.b + db;
+            let drdg = (0xF0 & drdg_dbdg) >> 4;
+            let dbdg = 0x0F & drdg_dbdg;
+            let dg = (0b00111111 & next_byte).wrapping_sub(32);
+            let dr = drdg.wrapping_add(dg).wrapping_sub(8);
+            let db = dbdg.wrapping_add(dg).wrapping_sub(8);
+            let r = prev_pixel.r.wrapping_add(dr);
+            let g = prev_pixel.g.wrapping_add(dg);
+            let b = prev_pixel.b.wrapping_add(db);
             write_array.push(r);
             write_array.push(g);
             write_array.push(b);
@@ -416,11 +432,11 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
                 r: r,
                 g: g,
                 b: b,
-                a: prev_pixel.a
+                a: prev_pixel.a,
             };
             prev_pixel_arr[hash_rgba(&prev_pixel)] = prev_pixel.clone();
         } else if next_byte >> 6 == 0b11 {
-            let run_length: _ = 0b00111111 | next_byte;
+            let run_length = (0b00111111 & next_byte) + 1;
             for _ in 0..run_length {
                 write_array.push(prev_pixel.r);
                 write_array.push(prev_pixel.g);
@@ -438,10 +454,42 @@ fn decode_qoif(qoi_data: ImageData) -> std::io::Result<()> {
     Ok(())
 }
 
+fn write_dummy() -> std::io::Result<()> {
+    let out_fname = "u.png";
+
+    let path = Path::new(out_fname);
+    let file = File::create(path)?;
+    let ref mut w = BufWriter::new(file);
+
+    let width: u8 = 255;
+    let height: u8 = 255;
+
+    let mut encoder = png::Encoder::new(w, width as u32, height as u32);
+    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_depth(png::BitDepth::Eight);
+
+    let mut writer = encoder.write_header()?;
+
+    let mut write_array: Vec<u8> = vec![];
+
+    for i in 0..width {
+        for j in 0..height {
+            write_array.push(127 * (j % 2) * (i % 2));
+            write_array.push(85 * (j % 3) * (i % 2));
+            write_array.push(63 * (j % 4) * (i % 2));
+        }
+    }
+
+    writer.write_image_data(&write_array).unwrap();
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     /*
      *PNG -> QOI -> PNG?
      */
+    write_dummy().unwrap();
     let image_data = get_encoding_file_data()?;
     match image_data.png_type {
         png::ColorType::Rgb => encode_qoif(image_data),
@@ -451,4 +499,105 @@ fn main() -> std::io::Result<()> {
     let qoif_image_data = get_decoding_file_data()?;
     decode_qoif(qoif_image_data)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::calc_qoif;
+    use crate::hash_rgba;
+    use crate::ImageData;
+    use crate::PixelValue;
+
+    fn gen_image_data(v: Vec<u8>, width: u32, height: u32, is_rgba: bool) -> ImageData {
+        ImageData {
+            png_type: if is_rgba {
+                png::ColorType::Rgba
+            } else {
+                png::ColorType::Rgb
+            },
+            bytes: v,
+            width: width,
+            height: height,
+            is_srgb: true,
+        }
+    }
+
+    #[test]
+    fn basic_rgb() {
+        let id = gen_image_data(vec![0x88, 0xFF, 0x88], 1, 1, false);
+        let out = calc_qoif(id).unwrap();
+        println!("{:?}", out);
+        assert!(out.len() == 14 + 4 + 8);
+        assert!(out[14] == 0xFE);
+        assert!(out[15] == 0x88);
+        assert!(out[16] == 0xFF);
+        assert!(out[17] == 0x88);
+    }
+
+    #[test]
+    fn basic_rgba() {
+        let id = gen_image_data(vec![0x12, 0x34, 0x56, 0x78], 1, 1, true);
+        let out = calc_qoif(id).unwrap();
+        println!("{:?}", out);
+        assert!(out.len() == 14 + 5 + 8);
+        assert!(out[14] == 0xFF);
+        assert!(out[15] == 0x12);
+        assert!(out[16] == 0x34);
+        assert!(out[17] == 0x56);
+        assert!(out[18] == 0x78);
+    }
+
+    #[test]
+    fn basic_diff() {
+        let id = gen_image_data(vec![1, 1, 1], 1, 1, false);
+        let out = calc_qoif(id).unwrap();
+        println!("{:?}", out);
+        assert!(out.len() == 14 + 1 + 8);
+        assert!(out[14] == 0x40 | (1 + 2) << 4 | (1 + 2) << 2 | (1 + 2) << 0);
+    }
+
+    #[test]
+    fn multi_diff() {
+        let id = gen_image_data(vec![1, 1, 1, 2, 2, 2, 0, 0, 0], 3, 1, false);
+        let out = calc_qoif(id).unwrap();
+        println!("{:?}", out);
+        assert!(out.len() == 14 + 3 + 8);
+        assert!(out[14] == 0x40 | 1 + 2 << 4 | 1 + 2 << 2 | 1 + 2 << 0);
+        assert!(out[15] == 0x40 | 1 + 2 << 4 | 1 + 2 << 2 | 1 + 2 << 0);
+        assert!(out[16] == 0x40 | 0 << 4 | 0 << 2 | 0 << 0);
+    }
+
+    #[test]
+    fn basic_index() {
+        let id = gen_image_data(
+            vec![10, 10, 10, 23, 255, 23, 96, 45, 33, 10, 10, 10],
+            4,
+            1,
+            false,
+        );
+        let out = calc_qoif(id).unwrap();
+        println!("{:?}", out);
+
+        let repeated_px = PixelValue {
+            r: 10,
+            g: 10,
+            b: 10,
+            a: 255,
+        };
+        let repeated_px_idx = hash_rgba(&repeated_px);
+
+        // luma + 2 * rgb + idx
+        let expected_file_len = 14 + 2 + 2 * 4 + 1 + 8;
+        assert!(
+            out.len() == expected_file_len,
+            "out len is {} expected {}",
+            out.len(),
+            expected_file_len
+        );
+        assert!(
+            out[24] == 0x00 | repeated_px_idx as u8,
+            "repeated_px_idx is {}",
+            repeated_px_idx
+        );
+    }
 }
